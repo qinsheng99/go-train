5ea0c8dd0e4804d272a7c47494752d37282ce68f
zjm <1076115376@qq.com>
2022-10-25 11:43:38 +0800

    fix req

--- api/handel/demo/luojia.go
+++ api/handel/demo/luojia.go
@@ -12,22 +12,41 @@ import (
 
 const luojia = "https://a2f051d4cabf45f885d7b0108edc9b9c.infer.ovaijisuan.com/v1/infers/e3f5d06e-4c1f-4b62-b2f4-123d6c22d244/infer/image"
 
+var luoc chan string
+
+func init() {
+	luoc = make(chan string, 1)
+	luoc <- luojia
+}
 func (h *Handle) Luojia(c *gin.Context) {
 	//  - 桶名：luojianet
 	//  - 路径：infer/{user_name}/input.png
+	select {
+	case luourl := <-luoc:
+		result, err := h.luojia(c, luourl)
+		luoc <- luourl
+		if err != nil {
+			common.Failure(c, err)
+			return
+		}
+		common.Success(c, result)
+	default:
+		common.Failure(c, errors.New("busy"))
+	}
+}
+
+func (h *Handle) luojia(c *gin.Context, url string) (string, error) {
 	username := c.Query("username")
 
 	body := fmt.Sprintf(`{"user_name":"%s"}`, username)
-
 	t := h.gettoken()
 	head := map[string]string{
 		"X-Auth-Token": t,
 	}
 
-	data, err := httprequest.Post(luojia, []byte(body), head, nil)
+	data, err := httprequest.Post(url, []byte(body), head, nil)
 	if err != nil {
-		common.Failure(c, err)
-		return
+		return "", err
 	}
 
 	var luojiares struct {
@@ -37,13 +56,11 @@ func (h *Handle) Luojia(c *gin.Context) {
 
 	err = json.Unmarshal(data, &luojiares)
 	if err != nil {
-		common.Failure(c, err)
-		return
+		return "", err
 	}
 	if luojiares.Status != 200 {
-		common.Failure(c, errors.New("推理失败"))
-		return
+		return "", errors.New("推理失败")
 	}
 
-	common.Success(c, luojiares)
+	return luojiares.Result, nil
 }
--- api/handel/demo/pangu.go
+++ api/handel/demo/pangu.go
@@ -2,6 +2,7 @@ package demo
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"strings"
 
@@ -12,7 +13,29 @@ import (
 
 const pu = "https://a2f051d4cabf45f885d7b0108edc9b9c.infer.ovaijisuan.com/v1/infers/302dc54c-144f-425d-b0e7-767d07f8d180/infer/text"
 
+var puc chan string
+
+func init() {
+	puc = make(chan string, 1)
+	puc <- pu
+}
+
 func (h *Handle) Pangu(c *gin.Context) {
+	select {
+	case panurl := <-puc:
+		result, err := h.pangu(c, panurl)
+		puc <- panurl
+		if err != nil {
+			common.Failure(c, err)
+			return
+		}
+		common.Success(c, result)
+	default:
+		common.Failure(c, errors.New("busy"))
+	}
+}
+
+func (h *Handle) pangu(c *gin.Context, url string) (string, error) {
 	q := c.Query("question")
 
 	t := h.gettoken()
@@ -24,10 +47,9 @@ func (h *Handle) Pangu(c *gin.Context) {
 		err  error
 	)
 
-	data, err = httprequest.Post(pu, []byte(fmt.Sprintf(`{"question":"%s"}`, q)), head, nil)
+	data, err = httprequest.Post(url, []byte(fmt.Sprintf(`{"question":"%s"}`, q)), head, nil)
 	if err != nil {
-		common.Failure(c, err)
-		return
+		return "", err
 	}
 
 	var pres = struct {
@@ -37,13 +59,12 @@ func (h *Handle) Pangu(c *gin.Context) {
 	err = json.Unmarshal(data, &pres)
 	if err != nil {
 		common.Failure(c, err)
-		return
+		return "", err
 	}
 	var result string = pres.Result
 
 	if i := strings.IndexByte(pres.Result, '\n'); i > 0 && q == pres.Result[:i] && i+1 < len(pres.Result) {
 		result = pres.Result[i+1:]
 	}
-
-	common.Success(c, result)
+	return result, nil
 }
